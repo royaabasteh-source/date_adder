d84aa99229e6a53f33fdec84c1b088ee5b3f2d46
royaabasteh-source <royaabasteh@gmail.com>
2026-03-14 14:31:07 +0200

    Create add_date.py

---
+++ add_date.py
@@ -0,0 +1,16 @@
+import os
+from datetime import date
+
+# مسیر پوشه‌ای که فایل‌ها هستند
+folder_path = "test_files"
+
+# گرفتن تاریخ امروز
+today = date.today().strftime("%Y-%m-%d")
+
+# رفتن روی هر فایل در پوشه
+for filename in os.listdir(folder_path):
+    if os.path.isfile(os.path.join(folder_path, filename)):
+        new_name = f"{today}_{filename}"
+        os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_name))
+
+print("All files have been renamed!")
